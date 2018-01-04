b7146239b47e706e7c696d87c837cd6cca40882b
EliaTB <34030726+EliaTB@users.noreply.github.com>
2018-01-04 09:36:22 +0100

    Add files via upload

---
+++ guard.py
@@ -0,0 +1,10 @@
+import pygame
+from maze import *
+
+class Guard:
+
+	def __init__(self)
+		self.position_x = position_x
+		self.position_y = position_y
+		self.x = position_x * spirte_size
+		self.y = position_y * sprite_size
