7d78f0aa2d6f67db6c5b7ec5d7760dffc6e69bce
EliaTB <34030726+EliaTB@users.noreply.github.com>
2018-01-04 09:44:50 +0100

    Update guard.py

--- guard.py
+++ guard.py
@@ -3,8 +3,13 @@ from maze import *
 
 class Guard:
 
-	def __init__(self)
-		self.position_x = position_x
-		self.position_y = position_y
-		self.x = position_x * spirte_size
-		self.y = position_y * sprite_size
+    def __init__(self)
+        self.position_x = position_x
+        self.position_y = position_y
+        self.x = position_x * spirte_size
+        self.y = position_y * sprite_size
+	
+    def items_check(self)
+#       if (char.items_counter) == 3:
+#           return True
+	
