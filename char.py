596a71feaae01cab5c0c5aebf0827f6b93227937
Elia <beneli.t@icloud.com>
2018-01-10 14:28:01 +0100

    Char movement update

--- char.py
+++ char.py
@@ -13,31 +13,35 @@ class Char:
 		
     def move_up(self)
 
-        if direction == "up"
-            position_y = position_y - 1
-            self.y = self.position_y * sprite_size
-            if structure_level[position_y][position_x] == "X" :
-                pass
+        if direction == "up" :
+            if self.position_y > 0 :
+                position_y = position_y - 1
+                self.y = self.position_y * sprite_size
+                if structure_level[position_y][position_x] == "X" :
+                    pass
          
     def move_down(self)
          
-       if direction == "down"
-            position_y = position_y + 1
-            self.y = self.position_y * spirte_size
-            if structure_level[position_y][position_x] == "X" :
-                pass
+       if direction == "down" :
+            if self.position_y < sprite_nb :       
+                position_y = position_y + 1
+                self.y = self.position_y * spirte_size
+                if structure_level[position_y][position_x] == "X" :
+                    pass
            
     def move_right(self)
            
-        if direction == "right"
-            position_x = position_x + 1
-            self.x = self.position_x * sprite_size
-            if structure_level[position_y][position_x] == "X" :
-                pass    
+        if direction == "right" :
+            if self.position_x > 0 :
+                position_x = position_x + 1
+                self.x = self.position_x * sprite_size
+                if structure_level[position_y][position_x] == "X" :
+                    pass    
 
     def move_left(self)
     
-    	if direction == "left"
+    	if direction == "left" :
+            if self.position_x < sprite_nb :
             position_x = position_x -1
             self.x = self.position_x * sprite_size
             if structure_level[position_y][position_x] == "X" :
